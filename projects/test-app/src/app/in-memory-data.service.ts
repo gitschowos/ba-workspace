4dd9ec283dad0ad1dd7ed76528dd1766608e62e2
Josef <J.Rothballer@web.de>
2021-08-10 14:41:11 +0200

    [json] create jsons for performance tests

--- projects/test-app/src/app/in-memory-data.service.ts
+++ projects/test-app/src/app/in-memory-data.service.ts
@@ -4,6 +4,11 @@ import { Observable } from 'rxjs';
 import mobile from '../../../../json/mobile-de.json';
 import specification from '../../../../json/specification.json';
 import testAll from '../../../../json/test-all.json';
+import empty from '../../../../json/empty.json';
+import performanceAll from '../../../../json/performance-all.json';
+import performanceCheckboxes from '../../../../json/performance-checkboxes.json';
+import performanceInputsHardcoded from '../../../../json/performance-inputs-hardcoded.json';
+import performanceInputsApi from '../../../../json/performance-inputs-api.json';
 
 @Injectable({
     providedIn: 'root'
@@ -19,6 +24,12 @@ export class InMemoryDataService implements InMemoryDbService {
         const engines = [
             'Diesel', 'Benzin', 'Gas', 'Wasserstoff', 'Elektro'
         ];
-        return { cities, engines, mobile, specification, testAll };
+        const colors = [
+            "rot", "grün", "blau", "gelb", "pink", "grau", "schwarz", "weiß", "braun", "magenta", "lila"
+        ];
+        return { cities, engines, colors, 
+            mobile, specification, testAll, empty,
+            performanceAll, performanceCheckboxes, performanceInputsHardcoded, performanceInputsApi
+        };
     }
 }

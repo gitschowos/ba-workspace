b742b91fc278a00986d9a9c71c3bc67181e04b43
Josef <J.Rothballer@web.de>
2021-09-24 13:29:07 +0200

    Extend setup script

--- setup.sh
+++ setup.sh
@@ -1,5 +1,22 @@
+#!/bin/bash
+# Setup for this workspace
+echo ===============================
+echo Installing dependencies...
+echo ===============================
 npm ci
+
+echo ===============================
+echo Building library...
+echo ===============================
 cd projects/ngx-model-driven-form
 npm run build
+
+echo ===============================
+echo Linking library...
+echo ===============================
 cd ../..
 npm link ./dist/ngx-model-driven-form
+
+echo ===============================
+echo Setup was successful. Have fun!
+echo ===============================
